a12868ddc70eab1d861a81652171192070376166
Aaron Addleman <aaronaddleman@gmail.com>
2015-12-21 21:38:26 -0800

    add projects file
    projects have many:
    tasks, environments, and jobs

---
+++ projects.rb
@@ -0,0 +1,9 @@
+# a project requires the following
+## tasks (defines the work to be executed)
+## environments (holds keys+values about environments)
+## jobs (takes the task and the environment and executes the work)
+
+
+# projects name
+# projects description
+# created at
