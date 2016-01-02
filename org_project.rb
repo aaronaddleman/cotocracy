f0023c8f54989b2ada1d59fcf3a8522856ff3523
Aaron Addleman <aaronaddleman@gmail.com>
2016-01-01 22:35:51 -0800

    modified org_project and runner to support multiple jobs
    by updating org_project.rb and runner.rb I was able to allow for multiple jobs to be executed in a row
    moved command token subsituion to the runner.rb for allowing runners to make the decision of which environment to use
    add idea for alternate method of executing commands on one line, but this makes for hard debugging

--- org_project.rb
+++ org_project.rb
@@ -16,6 +16,15 @@ header("Content-Type: text/plain");
 echo "Hello, world!\n";
 eos
 
+RESOLVE_CONF = <<-eos
+nameserver 8.8.8.8
+nameserver 8.8.4.4
+eos
+
+SSHPUBKEY = <<eos
+ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQD1dYX3Zbwa32fIaRKTpZb6eVLsJaNIOKAxN2U4jltbP65qjobJz2IUCNW/9CiemuLpp1HPq7rBravVoYo8JbrYdEmeD2AwTQMfoWQOLTtKDesHlwqH1sfjiV9UKyt+2qJ/lN5hKAjnhNWRs6B/g3iEwRQ7xDdSgrVVjP2zeR2VdteLN04zTUQid9Ec3wbjYLTZG2/hEseHwQ2JGXQgHt/UU+yTGFdO6NO5NryF2i92VnZ7tJg3ghpUBY2cI/eCGuf6rtC2mn4qGPMcLH6XymhZz0qNZ/+pqmKrhnu03CaZ6OMXqtaBgU1eFsp8KjimuFM4a2Y7/+N2eDbihY+a6w+nCCu/0ZIOdzm9sOOHwueeLuYBf0TYHnKbdpSNR8Di0EFiQJ+yNjyVXe1pqo8c2gXDxqVhyz8sjXBhgCGjwyZ0EQijkN5CbEGccxAmGfl/tIdMXlxx5PZMQpU+wVI2FVk/wonrXvdleT68T6c94ZRMyXzL+25gf9lBkKf/yGQjbOD1Uo8wHFbDabEiSjWcsBVvJ9/flBo2mMU8PeXBAiF43lOVc/B0D1sowkxafvhRSIHb15r4z9dHbxs/FWjeEAGIsImz+r5TMDn0RHk1tAZ7mzAlEAPHtefnmpAchZ/N033vGwEtGy6I1b5IC5c7b2JnUyY36TE/bru8YZ5wKGnAnw==
+eos
+
 # add dev environment
 dev = Environment.new(:name => "development", :type => :development)
 dev.add_variable(:keyname => "appdir", :role => "path", :value => "/var/www/html")
@@ -29,6 +38,19 @@ prod.add_variable(:keyname => "appfile", :role => "content", :value => PHPAPP)
 prod.add_variable(:keyname => "frontend1", :role => "instance", :value =>  {:fqdn => "frontend-001.internal.dev", :ipaddress => "54.158.155.138"})
 prod.add_variable(:keyname => "frontend2", :role => "instance", :value =>  {:fqdn => "frontend-002.internal.dev", :ipaddress => "54.92.232.190"})
 
+
+# add broken environment
+broken = Environment.new(:name => "broken", :type => :production)
+broken.add_variable(:keyname => "appdir", :role => "path", :value => "/var/www/html")
+broken.add_variable(:keyname => "appfile", :role => "content", :value => PHPAPP)
+broken.add_variable(:keyname => "frontend1", :role => "instance", :value =>  {:fqdn => "frontend-001.internal.dev", :ipaddress => "54.145.229.14"})
+broken.add_variable(:keyname => "dns", :role => "content", :value => RESOLVE_CONF)
+broken.add_variable(:keyname => "ssh", :role => "content", :value => SSHPUBKEY)
+
+test = Environment.new(:name => "broken", :type => :production)
+test.add_variable(:keyname => "appdir", :role => "path", :value => "/var/www/html")
+test.add_variable(:keyname => "appfile", :role => "content", :value => PHPAPP)
+test.add_variable(:keyname => "frontend1", :role => "instance", :value =>  {:fqdn => "frontend-001.internal.dev", :ipaddress => "10.0.1.172"})
 
 # add installation tasks
 installation_tasks = [
@@ -45,23 +67,33 @@ config_tasks = [
   Task.new(:name => "create logging directory", :command => "mkdir -p %%appdir-path%%/log")
 ]
 
+# fix issues
+fix_issues = [
+  # kill the process with signal SIGKILL to release the file for deletion
+  Task.new(:name => "stop service with open files marked for deletion", :command => "kill -SIGKILL `lsof -nP +L1 | awk '{print $2}' | tail -1`"),
+  # create remote .ssh/authorized_keys
+  Task.new(:name => "make .ssh", :command => "mkdir /root/.ssh"),
+  Task.new(:name => "create authorized_keys", :command => "echo -n '%%ssh-content%%' > /root/.ssh/authorized_keys"),
+  Task.new(:name => "set permissions", :command => "chmod 600 /root/.ssh && /root/.ssh/authorized_keys"),
+  # dns issue not working correctly
+  Task.new(:name => "populate the resolve.conf file with correct entries", :command => "echo -n '%%dns-content%%' > /etc/resolv.conf"),
+  Task.new(:name => "kill process listening on port 80", :command => "kill -SIGKILL `netstat -tulpn | grep ':80' | awk '{print $7}' | awk -F\/ '{print $1}'`")
+]
+
+
 # add a job for installation using installation_tasks and dev environment variables
 install_deps = Job.new(:name => 'install deps', 
                        :tasks => installation_tasks)
 
+fix_broken_host = Job.new(:name => 'fix issues',
+                          :tasks => fix_issues)
 
 # add tasks to project
+phpApp.add_job(fix_broken_host)
 phpApp.add_job(install_deps)
 
 # create runner object
-runner_prod = Runner.new(:environment => prod)
-runner_dev = Runner.new(:environment => dev)
-
-# execute tasks
-phpApp.jobs.each do |id,job|
-
-  job.tasks.each do |task|
-    runner_prod.execute(task.command)
-  end
-
-end
+# runner_prod = Runner.new(:environment => prod, :project => phpApp)
+# runner_dev = Runner.new(:environment => dev, :project => phpApp)
+runner_broken = Runner.new(:environment => broken, :project => phpApp).execute
+# runner_test = Runner.new(:environment => test, :project => phpApp)
--- runner.rb
+++ runner.rb
@@ -2,24 +2,62 @@ require 'logger'
 require 'open3'
 
 class Runner 
-  attr_accessor :environment, :log_file
+  attr_accessor :environment, :log_file, :project, :commands
 
   #shell to be used
   BASH_PATH = "/bin/bash"
+  #root password
+  ROOTPW = "foobarbaz"
 
 
   def initialize(opts={})
     @environment = opts[:environment] || nil
+    @commands = Array.new
+    @project = process_project(opts[:project]) || nil
     @logfile = Logger.new(opts[:log_file]) || Logger.new('log_file.log')
     @print = Logger.new(STDOUT)
   end
 
-  # allow executing on remote host
-  def execute(command)
-    @environment.instances.each do |instance|
+  # def add_command(command)
+  #   self.commands  =  "#{command}"
+  # end
+
+  def process_project(project)
+    extracted_commands = []
+    project.jobs.each do |id,job|
+      job.tasks.each do |task|
+        if task.command.match(/%%(.*)%%/)
+          task.command.match(/%%(.*)%%/).captures.each do |substrings|
+            begin
+              task.command = task.command.gsub(/%%.*%%/, @environment.variables[substrings])
+              # add_command(task.command)
+            rescue => e
+              raise "'#{substrings} is not defined in '#{environment.name}'"
+            end
+          end
+        end
+        extracted_commands << task.command
+      end
+    end
+
+    # add extracted commands to attribute    
+    @commands = extracted_commands
+    # return the processed project 
+    project
+  end
+
+  # execute commands
+  def execute
+    @commands.each do |command|
+      @environment.instances.each do |instance|
       @logfile.info("-----------------------")
       @logfile.info("executing: '#{command}'")
       @logfile.info("for node: '#{instance}'")
+
+      # create variable for capturing output or error
+      captured_stdout = ''
+      captured_stderr = ''
+
       # block for connecting to instance via ssh and passing the command from the task
       bashremote = lambda do |command|
         Open3.popen3("ssh -i id_rsa root@#{instance} #{BASH_PATH}") do |stdin, stdout, stderr|
@@ -29,18 +67,7 @@ class Runner
           @logfile.info(stdout.read)
           @logfile.error(stderr.read)
         end
-      end
 
-      # if the command has a substitution, replace the token from the selected environment's variables
-      if command.match(/%%(.*)%%/)
-        command.match(/%%(.*)%%/).captures.each do |substrings|
-          # if the environment is missing a variable, raise an error
-          begin
-            command = command.gsub(/%%.*%%/, @environment.variables[substrings])
-          rescue => e
-            raise "'#{substrings}' is not defined in '#{@environment.name}'"
-          end
-        end
       end
 
       # log the command
@@ -51,5 +78,40 @@ class Runner
       @logfile.info("-----------------------")
     end
 
+    end
+  end
+
+  # possible idea to execute all commands on one line, but really bad for debugging
+  def exec_bulk
+    command = @commands.join(" && ")
+
+    @environment.instances.each do |instance|
+
+      @logfile.info("-----------------------")
+      @logfile.info("executing: '#{command}'")
+      @logfile.info("for node: '#{instance}'")
+
+      # create variable for capturing output or error
+      captured_stdout = ''
+      captured_stderr = ''
+
+      # block for connecting to instance via ssh and passing the command from the task
+      bashremote = lambda do |command|
+        Open3.popen3("ssh -i id_rsa root@#{instance} #{BASH_PATH}") do |stdin, stdout, stderr|
+          stdin.puts command
+          stdin.close_write
+          puts stdout.read
+          @print.info( "\n" + stdout.read)
+          @logfile.info(stdout.read)
+          @logfile.error(stderr.read)
+        end
+      end
+
+      bashremote["#{command}"]
+    end
+
+    # puts "got:"
+    # puts command.inspect
+    # Open3.pipeline(commands)
   end
 end
